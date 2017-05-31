9fde046d051f7e2154c72461d64102400eb6ccb6
a2xl08 <axel.correia@etu.emse.fr>
2017-05-31 10:40:49 +0200

    FEAT: Importation de données csv

---
+++ PrototypePerso/js/displaydata.js
@@ -0,0 +1,12 @@
+var svg = d3.select("#vue").append("svg");
+
+d3.csv("data/speedfinal", function(data){
+	axe1 = data[0];
+	axe2 = data[1];
+	axe3 = data[2];
+
+	function displayaxe1(){
+		
+	}
+
+});
